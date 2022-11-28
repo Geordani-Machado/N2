9cedaa7fbc81cd64a56bd5b6219c89211f698bea
Geordani Machado <geordanimachado756@gmail.com>
2022-11-28 20:09:51 -0300

    CONSTRUÇÃO DO FRONT-END

---
+++ Front-end/src/view/Home.vue
@@ -0,0 +1,52 @@
+<template>
+    
+        <div className="p-5"> 
+        <h1 className="ml-7 text-slate-600 text-xl font-semibold">Ritter Locações</h1>
+         </div>
+
+         <div className="w-screen flex justify-content align-items">
+            <div className="w-10/12 mt-32 ml-32">
+                <h1 className="text-5xl mt-32 text-slate-800 font-bold">
+                    Ritter Locação de Carros.
+                </h1>
+                <h1 className="text-1xl  text-slate-800 font-bold">
+                    Os melhores preços para você alugar um carro.
+                </h1>
+            </div>
+            <div className="rounded-l-lg ...">
+                <img className="border-3" src="../assets/hero-banner.jpg"/>
+            </div>
+         </div>
+
+         <div>
+            <h1 className="text-1xl text-center mt-8 text-slate-800 font-bold animate-bounce">
+                    Ver Modelos
+                </h1>
+         </div>
+    
+     
+      
+        
+        
+      
+     
+      
+     
+      
+  </template>
+  
+  <script>
+
+
+  export default {
+    name: "HelloWorld",
+    props: {
+        msg: String
+    },
+    components: { }
+}
+  </script>
+  
+
+
+  
