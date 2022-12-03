7b27a6fc4858ce2b0d342e2f76ae7da88b3eb9cb
Geordani Machado <geordanimachado756@gmail.com>
2022-12-02 22:33:28 -0300

    alteração nas rotas

--- Front-end/src/components/Card.vue
+++ Front-end/src/components/Card.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="border-4 rounded-xl ml-1 mb-1">
-    <img v-bind:src="url" className="rounded-xl " style="height: 170px;"/>
-    <h1>Nome: {{ nome }}</h1>
-    <h1>Cor: {{ cor }}</h1>
-    <h1> Ano Fabricação: {{ ano }}</h1>
-    <button >Reservar</button>
+    <img v-bind:src="url" className="rounded-xl justify-center ml-3" style="width: 280px;"/>
+    <h1 class=" text-center text-slate-600 text-xl font-semibold">{{ nome }}</h1>
+    <div class="flex justify-center">
+      <h1 class="ml-2 mr-2">Cor: {{ cor }}</h1>
+      <h1 class="ml-2 mr-2"> | </h1>
+      <h1 class="ml-2 mr-2"> Ano Fabricação: {{ ano }}</h1>
+    </div>
+    <button class=" justify-center text-center ml-8 mt-2">Reservar</button>
    
   </div>
 </template>
